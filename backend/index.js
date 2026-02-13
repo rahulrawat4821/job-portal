43a9e9ef6b879821af8f77e479520df02120dacb
rahul rawat <rahulrawat84588@gmail.com>
2026-02-13 11:46:12 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -19,19 +19,17 @@ const allowedOrigins = [
   "https://job-portal-gj9j.netlify.app"
 ];
 
-app.use(cors({
-  origin:[
-    "http://localhost:5173",
-    "https://job-portal-gj9j.netlify.app"
-  ],
-  credentials: true
-}));
-
-
-
-
-
-
+// app.use(cors({
+//   origin:[
+//     "http://localhost:5173",
+//     "https://job-portal-gj9j.netlify.app"
+//   ],
+//   credentials: true
+// }));
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url );
+  next();
+});
 // 2. OTHER MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
