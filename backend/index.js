95a944f55b2ddc5d186eda25584d176762c868b1
rahul rawat <rahulrawat84588@gmail.com>
2026-02-13 11:49:43 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -26,10 +26,18 @@ const allowedOrigins = [
 //   ],
 //   credentials: true
 // }));
+
+//fix cors
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.url );
   next();
 });
+
+
+
+
+
+
 // 2. OTHER MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
